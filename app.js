592e0132e1ca63b8fe1e4e5e13854b137230bdff
farhansolodev <farhanalvi005@gmail.com>
2022-01-18 19:30:02 +0400

    added cors middleware and added dummy code for testing

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var cors = require("cors")
 const SERVICE_ACCOUNT_KEY = require("./keys/serviceAccountKey")
 const PORT = process.env.PORT || 3000
 
@@ -11,7 +12,6 @@ const firebaseApp = firebase.initializeApp(
 )
 
 firebaseApp && console.log(`Firebase app [${firebaseApp.name}] initialized... `)
-
 ;(module.exports.db = firebase.firestore(firebaseApp)) && console.log(`Connected to Firestore... `)
 
 // Initialize Express
@@ -20,11 +20,26 @@ const app = express()
 const http = require("http").createServer(app)
 module.exports.io = require("socket.io")(http)
 
+app.use(cors())
 app.use(express.json())
 
 try {
 	const virtualSpaceRoutes = require("./routes/virtualSpaceRoutes")
-	app.use("/vs", virtualSpaceRoutes)
+	// app.options("/vs", (req, res, next) => {
+	// 	console.log("OPTIONS /vs")
+	// 	res.status(200).send({ options: true })
+	// })
+	app.get("/vs", (req, res) => {
+		res.header("Access-Control-Allow-Origin", "*")
+		console.log(`User ${JSON.stringify(req.query["USER_ID"])} is trying to join ROOM [id: ${req.query["ROOM_ID"]}]` + ` Method: ${req.method}`)
+		res.status(200).send("VS JOINED")
+	})
+	app.post("/vs", (req, res) => {
+		res.header("Access-Control-Allow-Origin", "*")
+		console.log(`User ${JSON.stringify(req.body.USER_ID)} is trying to create ROOM [name:${req.body.ROOM_NAME}]` + ` Method: ${req.method}`)
+		res.status(200).send("VS CREATED")
+	})
+	// app.use("/vs", virtualSpaceRoutes)
 } catch (error) {
 	throw error
 }
