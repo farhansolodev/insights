4bb5400ab8d548749fdb0cb6677e2599abfc8b58
farhansolodev <farhanalvi005@gmail.com>
2022-02-07 16:10:00 +0400

    parseing the key

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 const admin = require("firebase-admin")
-// const SERVICE_ACCOUNT_KEY = require("./keys/serviceAccountKey")
+const SERVICE_ACCOUNT_KEY = JSON.parse(process.env.firebase_key)
 // const { initializeApp } = require("firebase-admin/app")
 const { getFirestore } = require("firebase-admin/firestore")
 
 const app = admin.initializeApp({
-  credential: admin.credential.cert(process.env.firebase_key)
+  credential: admin.credential.cert(SERVICE_ACCOUNT_KEY)
 }, 'virtual-spaces');
 console.log(`Firebase app [${app.name}] initialized...`)
 
